f70c97feeca8aae5ce833fc9f6a6eb0a96ed1241
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-07-16 21:34:11 +0430

    new(post.go): add post fields

---
+++ domain/post.go
@@ -0,0 +1,21 @@
+package domain
+
+import "time"
+
+type Post struct {
+	Id          string
+	Thumbnail   *File
+	Title       string
+	Content     string
+	FullContent string
+	Categories  []*Category
+	Tags        []string
+	Comments    []*Comment
+	Author      *Account
+	View        int
+	Like        int
+	PublishDate *time.Time
+	Status      PostStatus
+	CreateAt    *time.Time
+	UpdateAt    *time.Time
+}
