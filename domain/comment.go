6d3f9bef61754911b78f2dd53ed05fa141273165
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-07-21 12:19:50 +0430

    refactor(comment.go): fix typo error and remove pointer from CreateAt and UpdateAt

--- domain/comment.go
+++ domain/comment.go
@@ -3,14 +3,14 @@ package domain
 import "time"
 
 type Comment struct {
-	Id       string
-	Parent   *Comment
-	Children []*Comment
-	Post     *Post
-	Emain    string
-	Name     string
-	Content  string
-	Status   CommentStatus
-	CreateAt *time.Time
-	UpdateAt *time.Time
+	Id        string
+	Parent    *Comment
+	Children  []*Comment
+	Post      *Post
+	Email     string
+	Name      string
+	Content   string
+	Status    CommentStatus
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
