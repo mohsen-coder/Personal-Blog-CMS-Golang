ae845a71b62a90a41d3819ab4623c47eab5c4781
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-07-21 12:39:11 +0430

    update(main.go): add fiber server

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"mohsen-coder/PersonalBlog/config"
 
+	"github.com/gofiber/fiber/v2"
+)
+
+func homeRoute(c *fiber.Ctx) error {
+	return c.SendString("hello world")
+}
+
+func main() {
+	config.ConnectDb()
+	app := fiber.New()
+
+	app.Get("/", homeRoute)
+
+	log.Fatal(app.Listen(":3000"))
 }
