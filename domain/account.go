ac9a0dc4b04e738433b807ce80c2622036713c25
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-07-21 12:18:25 +0430

    refactor(account.go): fix typo error and delete pointer from CreatedAt and UpdatedAt

--- domain/account.go
+++ domain/account.go
@@ -9,11 +9,11 @@ type Account struct {
 	Name      string
 	Family    string
 	Username  string
-	Emial     string
+	Email     string
 	Password  string
 	About     string
 	Thumbnail *File
 	Role      AccountRole
-	CreateAt  *time.Time
-	UpdateAt  *time.Time
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
