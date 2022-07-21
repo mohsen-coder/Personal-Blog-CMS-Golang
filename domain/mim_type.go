17507e1e660f10671279a3a961de9235461347c6
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-07-21 12:28:33 +0430

    refactor&update(mim_type.go): rename none constant, add new file type, fill constant values

--- domain/mim_type.go
+++ domain/mim_type.go
@@ -3,10 +3,12 @@ package domain
 type MimType string
 
 const (
-	FileJPG MimType = "jpg"
-	FilePNG MimType = ""
-	FileGIF MimType = ""
-	FileZIP MimType = ""
-	FileRAR MimType = ""
-	FilePDF MimType = ""
+	FileNone MimType = "none"
+	FileJPG  MimType = "image/jpeg"
+	FilePNG  MimType = "image/png"
+	FileGIF  MimType = "image/gif"
+	FileZIP  MimType = "application/zip"
+	FileRAR  MimType = "application/vnd.rar"
+	FilePDF  MimType = "application/pdf"
+	FileText MimType = "text/plain"
 )
