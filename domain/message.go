7861dff44357e60fafcccee1e17e4749e340854b
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-07-21 12:23:51 +0430

    refactor(message.go): fix status and file types, remove pointer from CreatedAt and UpdatedAt

--- domain/message.go
+++ domain/message.go
@@ -3,16 +3,16 @@ package domain
 import "time"
 
 type Message struct {
-	Id       string
-	Name     string
-	Email    string
-	WebSite  string
-	Title    string
-	Content  string
-	Status   CommentStatus
-	Reply    *Message
-	Parent   *Message
-	File     *Message
-	CreateAt *time.Time
-	UpdateAt *time.Time
+	Id        string
+	Name      string
+	Email     string
+	WebSite   string
+	Title     string
+	Content   string
+	Status    MessageStatus
+	Reply     *Message
+	Parent    *Message
+	File      *File
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
